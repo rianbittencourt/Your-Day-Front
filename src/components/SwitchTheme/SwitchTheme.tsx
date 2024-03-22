914b3e6d6007948a6ea4df98be7332cee5670e04
rianbittencourt <rian.bitten2@gmail.com>
2024-03-22 20:22:28 -0300

    feat: Switch Theme ADD

---
+++ src/components/SwitchTheme/SwitchTheme.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Switch from "react-switch";
+import { useContext } from "react";
+import { ThemeContext } from "styled-components";
+
+interface Props {
+  toggleTheme: () => void;
+}
+
+export default function SwitchTheme({ toggleTheme }: Props) {
+  const { title } = useContext(ThemeContext);
+  return (
+    <div>
+      <Switch
+        onChange={toggleTheme}
+        checked={title === "dark"}
+        checkedIcon={false}
+        uncheckedIcon={false}
+        height={10}
+        width={30}
+        handleDiameter={20}
+        offColor="#000000"
+        onColor="#000000"
+        onHandleColor="#000000"
+        offHandleColor="#000000"
+      />
+    </div>
+  );
+}
