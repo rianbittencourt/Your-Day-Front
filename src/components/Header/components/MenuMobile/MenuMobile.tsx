c9bd69dfa4130968663723405972a48f86466365
rianbittencourt <rian.bitten2@gmail.com>
2024-03-28 06:19:50 -0300

    feat: MenuMobile Animation Conditional

--- src/components/Header/components/MenuMobile/MenuMobile.tsx
+++ src/components/Header/components/MenuMobile/MenuMobile.tsx
@@ -2,20 +2,55 @@ import { FaUser } from "react-icons/fa";
 import { IoMdSettings } from "react-icons/io";
 import { PiPencilSimpleLineFill } from "react-icons/pi";
 import { FaReadme } from "react-icons/fa6";
+import { FaHome } from "react-icons/fa";
 import SettingsButton from "@/components/SettingsButton/SettingsButton";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+interface MenuItem {
+  title: string;
+  icon: JSX.Element;
+  href: string;
+}
+
+const MenuItems: MenuItem[] = [
+  {
+    title: "Home",
+    icon: <FaHome className="text-3xl" />,
+    href: "/",
+  },
+  {
+    title: "Profile",
+    icon: <FaUser className="text-3xl" />,
+    href: "/profile",
+  },
+  {
+    title: "Write",
+    icon: <PiPencilSimpleLineFill className="text-3xl" />,
+    href: "/write",
+  },
+  {
+    title: "Read",
+    icon: <FaReadme className="text-3xl" />,
+    href: "/read",
+  },
+];
 
 export default function MenuMobile() {
+  const router = usePathname();
+
   return (
-    <ul className="text-primary  gap-2 font-medium sm:hidden mx-auto flex relative rounded z-10">
-      <li className="hover:text-[#1CC0A9] cursor-pointer -translate-y-7 delay-100 bg-[#1CC0A9]  rounded-full flex p-4 justify-center border-[10px] border-[#333] items-center  ">
-        <FaUser className="text-3xl" />
-      </li>
-      <li className="hover:text-[#1CC0A9]  z-20  cursor-pointer delay-100 ] border-[10px] border-[#222]  rounded-full flex p-4 justify-center items-center  ">
-        <PiPencilSimpleLineFill className="text-3xl" />
-      </li>
-      <li className="hover:text-[#1CC0A9] cursor-pointer delay-100   rounded-full flex p-4 border-[10px] border-[#222] justify-center items-center  ">
-        <FaReadme className="text-3xl" />
-      </li>
+    <ul className="text-primary  gap-2 font-medium sm:hidden mx-auto flex relative rounded  z-10">
+      {MenuItems.map((menuItem) => (
+        <li
+          className={` p-4 delay-100 hover:text-[#1CC0A9] border-[10px] rounded-full 
+        ${router === menuItem.href ? "-translate-y-8  border-bgColor bg-[#1CC0A9] hover:text-primary cursor-none" : " border-transparent"}
+      `}
+          key={menuItem.title}
+        >
+          <Link href={menuItem.href}>{menuItem.icon}</Link>
+        </li>
+      ))}
       <SettingsButton />
     </ul>
   );
