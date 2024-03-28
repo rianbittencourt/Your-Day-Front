8bc2b073d2236f58b7e1b608344f988b5a190bf3
rianbittencourt <rian.bitten2@gmail.com>
2024-03-28 08:52:53 -0300

    test

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,7 @@
+export default function Profile() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between sm:p-20 bg-primary">
+      <h1>teste</h1>
+    </main>
+  );
+}
