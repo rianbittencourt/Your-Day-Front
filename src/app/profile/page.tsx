42045a8deaf28134eea656019b26c5e066e3fd4a
rianbittencourt <rian.bitten2@gmail.com>
2024-03-29 09:02:27 -0300

    feat: profile page

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,7 +1,23 @@
+import StatsCount from "@/components/Stats/StatsCount";
+import { Button } from "@/components/ui/button";
+
 export default function Profile() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between sm:p-20 bg-primary">
-      
-    </main>
+    <div className=" max-w-7xl mx-auto sm:px-5 px-10 mb-[10rem] sm:mb-0">
+      <div className="mt-10  ">
+        <div className="border-b-2 pb-10 border-primary/20">
+          <StatsCount />
+        </div>
+
+        <div className="flex flex-col justify-center items-center mt-10">
+          <h1 className="text-2xl font-200 text-center">
+            Você ainda não escreveu nada, que tal começar?
+          </h1>
+          <Button variant="outline" className="mt-5">
+            Escrever
+          </Button>
+        </div>
+      </div>
+    </div>
   );
 }
