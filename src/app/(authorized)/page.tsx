c1fcdcb9ca6c5d0bcbdd6aee9a0011e77e617d36
rianbittencourt <rian.bitten2@gmail.com>
2024-03-31 23:45:14 -0300

    fix: login route

--- src/app/(authorized)/page.tsx
+++ src/app/(authorized)/page.tsx
@@ -1,9 +1,29 @@
-import LoginRegister from "@/components/LoginRegister/LoginRegister";
-import { useSession, getSession } from "next-auth/react";
+"use client";
+import StatsCount from "@/components/Stats/StatsCount";
+import { Button } from "@/components/ui/button";
+import { useSession } from "next-auth/react";
+
 export default function Home() {
+  const { data: session, status } = useSession();
+
+  console.log(session);
+
   return (
-    <div className="  h-screen flex justify-center items-center py-10 ">
-      <h1>logado</h1>
+    <div className=" max-w-7xl mx-auto sm:px-5 px-10 mb-[10rem] sm:mb-0">
+      <div className="mt-10  ">
+        <div className="border-b-2 pb-10 border-primary/20">
+          <StatsCount />
+        </div>
+
+        <div className="flex flex-col justify-center items-center mt-10">
+          <h1 className="text-2xl font-200 text-center">
+            Você ainda não escreveu nada, que tal começar?
+          </h1>
+          <Button variant="outline" className="mt-5">
+            Escrever
+          </Button>
+        </div>
+      </div>
     </div>
   );
 }
--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,13 +1,49 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import axios from "axios";
 
 const handler = NextAuth({
+  session: { strategy: "jwt" },
+  secret: process.env.NEXTAUTH_SECRET,
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID || '',
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
+      clientId: process.env.GOOGLE_CLIENT_ID || "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
     }),
   ],
-});
+  callbacks: {
+    async jwt({ token, account, user }: any) {
+      if (user && account) {
+        token.accessToken = account.access_token;
+        token.id = user?.id;
 
+        console.log(token.accessToken);
+
+        try {
+          const response = await axios.post(
+            "http://localhost:3001/users/login",
+            {
+              googleID: user.id,
+              name: user.name,
+              email: user.email,
+            },
+            {
+              headers: {
+                Authorization: `Bearer ${token.accessToken}`,
+              },
+            }
+          );
+          console.log(
+            "Requisição para o backend enviada com sucesso!",
+            response.data
+          );
+        } catch (error) {
+          console.error("Erro ao fazer requisição para o backend:", error);
+        }
+      }
+
+      return token;
+    },
+  },
+});
 export { handler as GET, handler as POST };
